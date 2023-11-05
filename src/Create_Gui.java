f3a538265f8112df14a3e6cf84a617ab6cc50cc9
Kamil Walega <walegakamil@gmail.com>
2023-11-05 12:39:19 +0100

    adding files

---
+++ src/Create_Gui.java
@@ -0,0 +1,13 @@
+
+
+import javax.swing.*;
+
+public class Create_Gui {
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setSize(640, 480);
+        frame.setVisible(true);
+    }
+}
+
